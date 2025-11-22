22aece636cd8f103e3f7e9763de4e71f1adf463c
iremseven <sevenirem@yahoo.com>
2025-11-22 22:56:44 +0300

    Update pytorch.org_optimizing_model_parameters.py

--- pytorch.org_optimizing_model_parameters.py
+++ pytorch.org_optimizing_model_parameters.py
@@ -74,15 +74,11 @@ optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
 def train_loop(dataloader, model, loss_fn, optimizer):
     size = len(dataloader.dataset)
-    # Set the model to training mode - important for batch normalization and dropout layers
-    # Unnecessary in this situation but added for best practices
     model.train()
     for batch, (X, y) in enumerate(dataloader):
-        # Compute prediction and loss
         pred = model(X)
         loss = loss_fn(pred, y)
-
-        # Backpropagation
+        
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
@@ -93,15 +89,11 @@ def train_loop(dataloader, model, loss_fn, optimizer):
 
 
 def test_loop(dataloader, model, loss_fn):
-    # Set the model to evaluation mode - important for batch normalization and dropout layers
-    # Unnecessary in this situation but added for best practices
     model.eval()
     size = len(dataloader.dataset)
     num_batches = len(dataloader)
     test_loss, correct = 0, 0
 
-    # Evaluating the model with torch.no_grad() ensures that no gradients are computed during test mode
-    # also serves to reduce unnecessary gradient computations and memory usage for tensors with requires_grad=True
     with torch.no_grad():
         for X, y in dataloader:
             pred = model(X)
@@ -127,4 +119,5 @@ print("Done!")
 
 
 #%%
+
 
